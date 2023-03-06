7c392c24f177b24f8d7cd6a9c9f0457fad100c57
root <447818666@qq.com>
2023-03-06 22:30:59 +0800

    webpack 钩子相关

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,6 +1,33 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+class APlugin {
+    apply(compiler) {
+        console.log("self plugin====>:APlugin")
+        compiler.hooks.beforeRun.tap("A", compiler => {
+            console.log("hook:" + "A")
+        });
+    }
+}
+
+class BPlugin {
+    apply(compiler) {
+        console.log("self plugin====>:BPlugin")
+        compiler.hooks.initialize.tap("B", compiler => {
+            console.log("hook:" + "B")
+        });
+    }
+}
+
+class CPlugin {
+    apply(compiler) {
+        console.log("self plugin====>:CPlugin")
+        compiler.hooks.beforeRun.tap("C", compiler => {
+            console.log("hook:" + "C")
+        });
+    }
+}
+
 
 module.exports = {
     mode: "development",
@@ -8,24 +35,18 @@ module.exports = {
         index: {
             import: './src/index.js',
         },
-        // another: {
-        //     import: './src/another.js',
-        // },
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true
     },
-    // optimization: {
-    //     runtimeChunk: 'single',
-    //     splitChunks: {
-    //         chunks: 'all',
-    //     },
-    // },
     plugins: [
+        new APlugin(),
+        new BPlugin(),
+        new CPlugin(),
         new HtmlWebpackPlugin({
             title: '管理输出',
-        }),
+        })
     ],
 }
